8935822140d429c0b1e5e05d875ef5a79c663b85
kosuke miyoshi <miyosuda@gmail.com>
2023-12-11 00:45:28 +0900

    refactor interface for TextInput

--- test/test-input.test.ts
+++ test/test-input.test.ts
@@ -3,16 +3,13 @@ import path from 'path';
 import { describe, it, expect } from 'vitest';
 import { TextInput } from '../src/text-input';
 import { Config } from '../src/config';
-import { MapManager } from '../src/map-manager';
 import { Node } from '../src/node';
 
 
 describe('TextInput', () => {
-  it('can show and hide', () => {
+  it('can show, and can hide', () => {
     const html = fs.readFileSync(path.resolve(__dirname, '../src/index.html'), 'utf8');
     document.body.innerHTML = html;
-    
-    const mapManager = new MapManager();
     
     const g = document.getElementById('overlay');
     const config = new Config();
@@ -21,12 +18,19 @@ describe('TextInput', () => {
     const rightRootNode = new Node(null, g, config);
     rightRootNode.setText('root');
     
-    const textInput = new TextInput(mapManager);
+    const onTextDecidedCallback = (node : Node, changed : boolean) => {
+      expect(node.text).toBe('test_text');
+      expect(changed).toBe(true);
+    };
+    
+    const textInput = new TextInput(onTextDecidedCallback);
     textInput.applyConfig(config);
     
     textInput.show(rightRootNode);
     
     expect(textInput.isShown).toBe(true);
+    
+    textInput.input.value = 'test_text'
     
     textInput.onTextChange();
     
